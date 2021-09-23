5b3dd72bfe6493bb1fbbdf0964ba40b241f2f8e8
gopancakes <samuelfdove@gmail.com>
2021-09-23 08:41:53 -0500

    foundoutwhyitfailed

--- pythonevalfunctionmaybe.py
+++ pythonevalfunctionmaybe.py
@@ -1,6 +1,7 @@
 import chess
 import chess.pgn
 import io
+from copy import deepcopy
 board1 = chess.Board()
 #print(board1.fen())
 
@@ -37,16 +38,11 @@ def materialadvantagecalc(FEN):
 def evalpos(boardd):
     if boardd.is_game_over():
         result = boardd.outcome().result()
-        print(boardd)
-        print('ahh')
-        print(result)
         if result == "1-0":
             return 1000
         elif result == "0-1":
             return -1000
         else:
-            print(boardd.outcome())
-            print('AHHHHHHHHHHHHHHHHHHHHHHHHHHH')
             if boardd.turn==chess.WHITE:
                 return -100
             else:
@@ -58,32 +54,25 @@ def findsinglebestmove(boardd,depth,currentdepth=0):
     legalmoves = list(boardd.legal_moves)
     bestmovenumberarray = []
     for i in range(len(legalmoves)):
-        tempboard = chess.Board(boardd.fen())
+        tempboard = deepcopy(boardd)
         tempboard.push(legalmoves[i])
         if depth==currentdepth:
             bestmovenumberarray.append(evalpos(tempboard))
         else:
             if tempboard.is_game_over():
                 bestmovenumberarray.append(evalpos(tempboard))
-                print('HERE')
-                print(bestmovenumberarray)
             else:
                 bestmovenumberarray.append(findsinglebestmove(tempboard,depth,currentdepth+1))
-
-        #if currentdepth==0:
-            #print(i)
-            #print(len(legalmoves))
-    
     
     if currentdepth == 0:
+
         if boardd.turn==chess.WHITE:
             besteval = (max(bestmovenumberarray))
         else:
             besteval = (min(bestmovenumberarray))
-        print(bestmovenumberarray)
-        print(boardd.legal_moves)
         return legalmoves[bestmovenumberarray.index(besteval)]
     else:
+
         if boardd.turn==chess.WHITE:
             return max(bestmovenumberarray)
         else:
@@ -113,7 +102,7 @@ game.setup(board4)
 node = game
 
 
-'''while board4.is_game_over()==False:
+while board4.is_game_over()==False:
     bestmove = (findsinglebestmove(board4,2))
     node = node.add_variation(bestmove)
     board4.push(bestmove)
@@ -129,8 +118,8 @@ print(board4.outcome().termination)
 print(board4.outcome().result())
 print('#######################')
 print('DONE')
-print(game)'''
-
+print(game)
+'''
 pgn = io.StringIO("1. Nh3 Nh6 2. Ng5 Ng4 3. Nxh7 Rxh7 4. Rg1 Nxh2 5. Rh1 Nf3+ 6. gxf3 Rxh1 7. Nc3 Nc6 8. Nd5 Rb8 9. Nxe7 Bxe7 10. Rb1 Kf8 11. Ra1 Kg8 12. Rb1 Kh8 13. Ra1 Kg8 14. Rb1 Kh8 15. Ra1 Kg8 16. Rb1 Kh8 17. Ra1 Kg8 18. Rb1 Kh8 19. Ra1 Kg8 *")
 game = chess.pgn.read_game(pgn)
 #print(game.end().board())
@@ -138,5 +127,7 @@ board = game.end().board()
 board.pop()
 print(board)
 board.push_uci("h8g8")
+print(evalpos(board))
 print(board.is_game_over())
 print(board)
+'''
