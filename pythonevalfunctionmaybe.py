60bfda8846a53b9ec64592b6a08553e1eafae778
gopancakes <samuelfdove@gmail.com>
2021-09-24 21:14:15 -0500

    t

--- pythonevalfunctionmaybe.py
+++ pythonevalfunctionmaybe.py
@@ -1,3 +1,4 @@
+from re import A
 import chess
 import chess.pgn
 import io
@@ -6,10 +7,11 @@ import random
 import chess.polyglot
 import time
 
-printlogs = False
+printlogs = True
 
 board1 = chess.Board()
 #print(board1.fen())
+log2 = open('log3.txt','w')
 if printlogs: log = open('log.txt','w')
 if printlogs: print("LOG IS NEW",file=log)
 reader = chess.polyglot.open_reader("baron30.bin")
@@ -54,8 +56,104 @@ def materialadvantagecalc(FEN):
     if printlogs: print('calculated material advantage of ', FEN, 'as', materialadvantage,file=log)
     return materialadvantage
 
+def findlocationsofpieces(boardd):
+    whitelocs = []
+    blacklocs = []
+    allwhitelocs = chess.SquareSet()
+    allblacklocs = chess.SquareSet()
+
+    for i in range(1,7):
+        white = boardd.pieces(i,chess.WHITE)
+        black = boardd.pieces(i,chess.BLACK)
+        whitelocs.append(white)
+        blacklocs.append(black)
+        allwhitelocs.union(white)
+        allblacklocs.union(black)
+
+    return whitelocs,blacklocs,allwhitelocs,allblacklocs
+
+def findattacks(boardd,whitelocs,blacklocs):
+    whiteattacks = []
+    whiteattackers = []
+    whitedefenders = []
+    blackattacks = []
+    blackattackers = []
+    blackdefenders = []
+
+    for i in range(len(whitelocs)):
+        tempattacks = []
+        tempattackers = []
+        tempdefenders = []
+        for j in range(len(list(whitelocs[i]))):
+            tempattacks.append(boardd.attacks(list(whitelocs[i])[j]))
+            tempattackers.append(boardd.attackers(chess.BLACK,list(whitelocs[i])[j]))
+            tempdefenders.append(boardd.attackers(chess.WHITE,list(whitelocs[i])[j]))
+        whiteattacks.append(tempattacks)
+        whiteattackers.append(tempattackers)
+        whitedefenders.append(tempdefenders)
+    
+    for i in range(len(blacklocs)):
+        tempattacks = []
+        tempattackers = []
+        tempdefenders = []
+        for j in range(len(list(blacklocs[i]))):
+            tempattacks.append(boardd.attacks(list(blacklocs[i])[j]))
+            tempattackers.append(boardd.attackers(chess.WHITE,list(blacklocs[i])[j]))
+            tempdefenders.append(boardd.attackers(chess.BLACK,list(whitelocs[i])[j]))
+        blackattacks.append(tempattacks)
+        blackattackers.append(tempattackers)
+        blackdefenders.append(tempdefenders)
+
+    return whiteattacks, whiteattackers, whitedefenders, blackattacks,blackattackers, blackdefenders
+
+def findhanging(whiteattackers,whitedefenders,blackattackers,blackdefenders):
+    whitehanging = []
+    blackhanging = []
+    sumhanging = 0
+    
+
+    for i in range(len(whiteattackers)):
+        for j in range(len(whiteattackers[i])):
+            if len(list(whiteattackers)) >0 and len(list(whitedefenders)==0):
+                    sumhanging += piecevalues[i]
+
+    for i in range(len(blackattackers)):
+        for j in range(len(blackattackers[i])):
+            if len(list(blackattackers)) >0 and len(list(blackdefenders)==0):
+                    sumhanging -= piecevalues[i]
+
+    return sumhanging
+
+def findattackadv(boardd,whiteloc,whiteattackers,whitedefenders,blackloc,blackattackers,blackdefenders): #FINISH THIS
+    attackadv = 0
+    for i in range(len(whiteattackers)):
+        for j in range(len(whiteattackers[i])):
+            attackerslist = list(whiteattackers[i][j])
+            if len(attackerslist) != 0:
+                defenderslist = list(whitedefenders[i][j])
+
+                #find the material value of all the attackers and defenders
+                attackersvalues = []
+                for k in range(len(attackerslist)):
+                    attackersvalues.append(piecevalues[boardd.piece_type_at(attackerslist[k])])
+                attackersvalues.sort()
+
+                defendersvalues = []
+                for k in range(len(defenderslist)):
+                    defendersvalues.append(piecevalues[boardd.piece_type_at(defenderslist[k])])
+                defendersvalues.sort()
+                defendersvalues.insert(0,piecevalues[boardd.piece_type_at(list(whiteloc[i][j]))])
+
+                attackadv = 0
+                for k in range([len(attackersvalues),len(defendersvalues)].min()):
+                    attackadv
 
 
+
+
+
+                
+                
 
 def evalpos(boardd):
     #print('evaluating postion',boardd.move_stack,file=log)
@@ -100,9 +198,7 @@ def evalpos(boardd):
             allwhiteloc = allwhiteloc.union(boardd.pieces(i,chess.WHITE))
             allblackloc = allblackloc.union(boardd.pieces(i,chess.BLACK))
 
-        #set of squares that the color is attacking
-        whiteattacks = chess.SquareSet()
-        blackattacks = chess.SquareSet()
+
         
         #is king pinned
         isblackpinned = False
@@ -112,9 +208,6 @@ def evalpos(boardd):
         for i in range(len(allwhiteloc)):
             pieceloc = list(allwhiteloc)[i]
             
-            #attacks
-            whiteattacks = whiteattacks.union(boardd.attacks(pieceloc))
-            
             #is pinned
             if boardd.is_pinned(chess.WHITE,pieceloc):
                 iswhitepinned=True
@@ -122,7 +215,6 @@ def evalpos(boardd):
         #same for black pieces
         for i in range(len(allblackloc)):
             pieceloc = list(allblackloc)[i]
-            blackattacks = blackattacks.union(boardd.attacks(pieceloc))
             if boardd.is_pinned(chess.BLACK,pieceloc):
                 isblackpinned=True
 
@@ -137,10 +229,12 @@ def evalpos(boardd):
             avgblackpawnrank += (7-chess.square_rank(list(allblackpawns)[i]))/len(allblackpawns)
 
 
-        evalu = materialadvantagecalc(boardd.fen())*materialadvmult
-        evalu = evalu+(len(whiteattacks)*-len(blackattacks))*50
-        evalu = evalu+isblackpinned*300+iswhitepinned*-300
-        evalu += avgwhitepawnrank*75-avgblackpawnrank*75
+        evalu = materialadvantagecalc(boardd.fen())*1000
+        if printlogs: print("evalu after materialadv adjustment:",evalu,file=log)
+        evalu = evalu+(isblackpinned-iswhitepinned)*100
+        if printlogs: print("evalu after ispinned adjustment:",evalu,file=log)
+        evalu += (avgwhitepawnrank-avgblackpawnrank)*30
+        if printlogs: print("evalu after pawnforwardship adjustment:",evalu,file=log)
         timespenteval += time.time()-t2
         return evalu
 
@@ -167,13 +261,97 @@ def sortmoves(boardd):
     
     return sortedlegalmoves
 
+def pruning2(boardd,depth):
+
+    
+
+    tempboard = boardd.copy()
+    legalmoves = list(boardd.legal_moves)
+    prunedlegalmoves = list()
+    morepruned = list()
+    turn = boardd.turn
+    evals = []
+    isrep = []
+    toprun = []
+    for i in range(len(legalmoves)):
+        tempboard.push(legalmoves[i])
+        evals.append(evalpos(tempboard))
+        if tempboard.is_repetition(1):
+            isrep.append(True)
+        else: isrep.append(False)
+        tempboard.pop()
+
+    #depthvals
+    numfordepth = [len(evals),max([round(len(evals)/2),5]),max([round(len(evals)/4),4]),4,2,2,2,2,2]
+    
+    for i in range(min([numfordepth[depth],len(evals)])):#round(len(evals)/2)+1):
+        if turn==chess.WHITE:
+            index = evals.index(max(evals))
+        else:
+            index = evals.index(min(evals))
+        prunedlegalmoves.append(legalmoves[index])
+        if not isrep[index]:
+            morepruned.append(legalmoves[index])
+        legalmoves.pop(index)
+        evals.pop(index)
+    
+    if len(morepruned)==0: 
+        return prunedlegalmoves
+
+    return morepruned
+            
+        
+
+
+def pruning(boardd):
+
+    tempboard = boardd.copy()
+    legalmoves = list(boardd.legal_moves)
+    prunedlegalmoves = list()
+    turn = boardd.turn
+
+    for i in range(len(legalmoves)):
+        toprune = False
+        tempboard.push(legalmoves[i])
+
+        if tempboard.is_repetition(1):
+            toprune = True
+        else:
+            '''
+            whitelocs,blacklocs,allwhitelocs,allblacklocs = findlocationsofpieces(tempboard)
+            whiteattacks, whiteattackers, whitedefenders, blackattacks,blackattackers, blackdefenders = findattacks(boardd,whitelocs,blacklocs)
+            sumhanging = findhanging(whiteattackers,whitedefenders,blackattackers,blackdefenders)
+            changeinmaterial = materialadvantagecalc(tempboard)-materialadvantagecalc(boardd)
+            
+            if turn==chess.WHITE:
+                if sumhanging >= changeinmaterial:
+                    toprune = True
+            else:
+                if sumhanging <= changeinmaterial:
+                    toprune = True
+            '''
+            #add the stuff above to eval, and then go from there
+
+            #if not toprune:
+
+
+
+        if not toprune:
+            prunedlegalmoves.append(legalmoves[i])
+
+        tempboard.pop()
+    
+    return legalmoves
+    
+
+
 
 def findsinglebestmove(boardd,depth,currentdepth=0,minormaxofabove=None):
     
     if depth==currentdepth:
         legalmoves = list(boardd.legal_moves)
     else:
-        legalmoves = sortmoves(boardd)
+        legalmoves = pruning2(boardd,currentdepth)
     
     
     bestmovenumberarray = []
@@ -203,7 +381,6 @@ def findsinglebestmove(boardd,depth,currentdepth=0,minormaxofabove=None):
                 evalofmove = (findsinglebestmove(tempboard,depth,currentdepth+1,minormaxofthis))
                 if printlogs: print('EXITING LEVEL, eval:',evalofmove,file=log)
 
-        evalofmove -= 1
         bestmovenumberarray.append(evalofmove)
         if printlogs: print("depth:",currentdepth,"turn:",tempboard.ply(),"bestmovenumberarray now:",bestmovenumberarray,"for moves",legalmoves,file=log)
 
@@ -213,7 +390,7 @@ def findsinglebestmove(boardd,depth,currentdepth=0,minormaxofabove=None):
             minormaxofthis = max(bestmovenumberarray)
             if printlogs: print("taking max to find minormaxofthis",file=log)
             if minormaxofabove!=None:
-                if evalofmove > minormaxofabove:
+                if evalofmove-50 > minormaxofabove:
                     l=True
                     if printlogs: print('exiting loop because eval of ',evalofmove,"is greater than",minormaxofabove,file=log)
                     global totalbreaks
@@ -223,7 +400,7 @@ def findsinglebestmove(boardd,depth,currentdepth=0,minormaxofabove=None):
             minormaxofthis = min(bestmovenumberarray)
             if printlogs: print('taking min to find minormaxofthis',file=log)
             if minormaxofabove!=None:
-                if evalofmove < minormaxofabove:
+                if evalofmove+50 < minormaxofabove:
                     l=True
                     if printlogs: print('exiting loop because eval of ',evalofmove,"is less than",minormaxofabove,file=log)
                     totalbreaks += 1
@@ -234,16 +411,19 @@ def findsinglebestmove(boardd,depth,currentdepth=0,minormaxofabove=None):
     if currentdepth == 0:
         if boardd.turn==chess.WHITE:
             besteval = (max(bestmovenumberarray))
+            if printlogs: print('white move, taking max of bestmovenumberarray:', besteval,file=log)
         else:
             besteval = (min(bestmovenumberarray))
+            if printlogs: print('black move, taking min of bestmovenumberarray:', besteval,file=log)
         #return legalmoves[bestmovenumberarray.index(besteval)]
         
         locationsofbest = [index for index, element in enumerate(bestmovenumberarray) if element == besteval]
         #locationsofbest = bestmovenumberarray.index(besteval)
         if printlogs: print('found best moves at:',locationsofbest,"with eval:",besteval,file=log)
-        if printlogs: print("SENDING MOVE",boardd.ply(),":", legalmoves[locationsofbest],file=log)
+        randomnum = random.randint(0,len(locationsofbest)-1)
+        if printlogs: print("SENDING MOVE",boardd.ply()+1,":", legalmoves[locationsofbest[randomnum]],file=log)
         #return(legalmoves[locationsofbest])
-        return legalmoves[locationsofbest[random.randint(0,len(locationsofbest)-1)]]
+        return legalmoves[locationsofbest[randomnum]]
     else:
         if boardd.turn==chess.WHITE:
             return max(bestmovenumberarray)
@@ -260,11 +440,12 @@ def playmove(boardd,nodee):
 
     t = time.time()
     if reader.get(boardd) != None:
+        #print('bookmove')
         bestmove = reader.choice(boardd).move
         nodee = nodee.add_variation(bestmove)
-        #print('doingbookmove')
+        print('doingbookmove')
     else:
-        bestmove = (findsinglebestmove(boardd,2))
+        bestmove = (findsinglebestmove(boardd,4))
         nodee = nodee.add_variation(bestmove)
         
     boardd.push(bestmove)
@@ -282,28 +463,30 @@ attacksmult = 50
 pinnedmult = 300
 pawnranksmult = 75
 
+piecevalues = [1,3,3,5,9,100]
+
+for i in range(1):
+    board4 = chess.Board()
+    game = chess.pgn.Game()
+    game.setup(board4)
+    node = game
+    movenum = 0
+
+    while not board4.is_game_over():
+        movenum +=1
+        board4,node = playmove(board4,node)
+        print(game)
+        print('')
+        #print(game,file=log2)
 
 
-board4 = chess.Board()
-game = chess.pgn.Game()
-game.setup(board4)
-node = game
-
-
-while not board4.is_game_over():
-    board4,node = playmove(board4,node)
+    print(board4.outcome())
+    print(board4.is_game_over())
+    print(board4.outcome().termination)
+    print(board4.outcome().result())
+    print('#######################')
+    print('DONE')
     print(game)
-    print('')
-    print('')
-
-
-print(board4.outcome())
-print(board4.is_game_over())
-print(board4.outcome().termination)
-print(board4.outcome().result())
-print('#######################')
-print('DONE')
-print(game)
 
 
 
