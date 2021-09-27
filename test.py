da2205324793f68efdd5a47549055a75c3d352d4
gopancakes <samuelfdove@gmail.com>
2021-09-27 18:09:05 -0500

    test

--- test.py
+++ test.py
@@ -3,16 +3,27 @@ import io
 import chess.pgn
 import chess.polyglot
 
-pgn = io.StringIO("1. e4 N")
-pgn = io.StringIO("1. Nh3 Nh6 2. a3")
+
+#pgn = io.StringIO("1. e4 ")
+#pgn = io.StringIO("1. Nh3 Nh6 2. a3")
+pgn = io.StringIO("1. d4 f5 2. g3 Nf6 3. Bg2 g6 4. Nf3 d6 5. b4 Ne4 6. Nfd2 e5 7. f3 Qg5 8. Nxe4 Qh5 9. Nf6+ Ke7 10. Nxh5 a5 11. Nd2 f4 12. bxa5 Rxa5 13. gxf4 Nc6 14. d5 Nd4 ")
+
 game = chess.pgn.read_game(pgn)
 #print(game.end().board())
 boardd = game.end().board()
-boardd = chess.Board()
+#boardd = chess.Board()
 
-whitepawns = boardd.pieces(1,chess.WHITE)
-print(list(whitepawns))
-print(list(chess.square_rank(whitepawns)))
+print(boardd)
+print((list(boardd.legal_moves)))
+#print(list(boardd.pop.legal_moves)
+legalmoves = list(boardd.legal_moves)
 
+print(boardd.is_capture(legalmoves[0]))
 print('')
-print(chess.square_rank(list(whitepawns)[0]))
+print('')
+movestack = boardd.move_stack
+
+print(movestack)
+print(len(movestack))
+print(movestack[0].from_square)
+
